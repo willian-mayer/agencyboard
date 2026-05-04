f27b10bea8f9df19b57f2456eb8ff0e3b7db54bd
willian-mayer <willian.mayer.br@gmail.com>
2026-05-04 10:55:57 -0300

    creating database connection

---
+++ backend/agencyboard/src/main/java/com/agencyboard/agencyboard/AgencyboardApplication.java
@@ -0,0 +1,13 @@
+package com.agencyboard.agencyboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgencyboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgencyboardApplication.class, args);
+	}
+
+}
---
+++ backend/agencyboard/src/test/java/com/agencyboard/agencyboard/AgencyboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agencyboard.agencyboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgencyboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
